8d97ace2bd4bd46e36e2bad7077ba99879a54196
PaulAjii <paulajii134@gmail.com>
2023-12-25 18:19:45 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// function validateForm() {
+//   var name = document.getElementById('name').value;
+//   var email = document.getElementById('email').value;
+//   var message = document.getElementById('message').value;
+
+//   if (name === '' || email === '' || message === '') {
+//       alert('Please fill in all fields');
+//   } else {
+//       alert('Form submitted successfully!');
+//       // You can add code here to submit the form data to a server or perform other actions
+//   }
+// }
+
+console.log("Oke Alafia!!!");
